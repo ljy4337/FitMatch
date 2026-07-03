ec36a49f5a41ddf9ccb36294671f69d0fc5eefc3
ljy4337@gmail.com <ljy4337@gmail.com>
2026-07-03 22:16:18 +0900

    Initial Commit

---
+++ FitMatch/FitMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitMatchApp.swift
+//  FitMatch
+//
+//  Created by 이진영 on 7/3/26.
+//
+
+import SwiftUI
+
+@main
+struct FitMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FitMatchTests/FitMatchTests.swift
@@ -0,0 +1,17 @@
+//
+//  FitMatchTests.swift
+//  FitMatchTests
+//
+//  Created by 이진영 on 7/3/26.
+//
+
+import Testing
+@testable import FitMatch
+
+struct FitMatchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
